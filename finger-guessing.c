eccb071fa47209d90ef8cfbfa7b6a7dc67b193f9
chenweixuan-star <3394059798@qq.com>
2025-10-17 12:45:47 +0800

    fourth save

--- finger-guessing.c
+++ finger-guessing.c
@@ -2,13 +2,28 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<Windows.h>
+
+volatile int timeout_flag=0;
+
+VOID CALLBACK TimeoutHandler(HWND hwnd,UINT uMsg,UINT_PTR idEvent,DWORD dwTime){
+    timeout_flag=1;
+    KillTimer(NULL,idEvent);
+}
 
 int main(){
+    UINT_PTR timer_id;
     srand(time(NULL));
     int user,computer;
 
     printf("猜拳游戏:0-石头,1-剪刀,2-布\n");
+    printf("限时10秒内获胜,否则退出.\n");
+    timer_id=SetTimer(NULL,0,1000*10,TimeoutHandler);
     while(1){
+        if(timeout_flag){
+            printf("\n时间到!游戏结束。\n");
+            break;
+        }
         printf("请输入你的选择(0/1/2):");
         scanf("%d",&user);
 
@@ -17,6 +32,7 @@ int main(){
 
         if((user==0 && computer==1)||(user==0 && computer==2)||(user==2 && computer==0)){
             printf("你赢了！游戏结束。\n");
+            KillTimer(NULL,timer_id);
             break;
         }
         else if(user==computer){
@@ -24,6 +40,11 @@ int main(){
         }else{
             printf("你输了，继续努力！\n");
         }
+        Sleep(100);
+        MSG msg;
+        PeekMessage(&msg,NULL,0,0,PM_REMOVE);
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
     }
     return 0;
 }
